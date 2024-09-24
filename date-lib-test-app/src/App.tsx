a0992d45ea7a44666def258517e958bd3faed9d3
joemiddie13 <joemiddie13@gmail.com>
2024-09-23 23:14:10 -0700

    React app works if I exit out of the error screen

---
+++ date-lib-test-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import DateDisplay from './components/DateDisplay';
+
+function App() {
+  return (
+    <div className="App">
+      <DateDisplay />
+    </div>
+  );
+}
+
+export default App;
---
+++ date-lib-test-app/src/components/DateDisplay.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import D from 'date-lib';
+
+const DateDisplay: React.FC = () => {
+  const [date, setDate] = useState(new D());
+
+  const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDate(new D(event.target.value));
+  };
+
+  return (
+    <div>
+      <h1>Date Library Demo</h1>
+      <input type="date" onChange={handleDateChange} />
+      <p>Formatted Date: {date.format('Y-M-D')}</p>
+      <p>When: {date.when()}</p>
+    </div>
+  );
+};
+
+export default DateDisplay;
