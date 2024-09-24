510b40c900b19396a8412b4bd18268155ae2a6ee
joemiddie13 <joemiddie13@gmail.com>
2024-09-24 10:29:01 -0700

    Bundled with Rollup

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
+import dts from 'rollup-plugin-dts';
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/index.js',
+        format: 'cjs',
+      },
+      {
+        file: 'dist/index.esm.js',
+        format: 'es',
+      },
+    ],
+    plugins: [
+      resolve(),
+      typescript(),
+    ],
+  },
+  {
+    input: 'dist/dts/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'es' }],
+    plugins: [dts()],
+  },
+];
