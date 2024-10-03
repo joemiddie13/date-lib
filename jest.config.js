2b6c01c41397f2d558a70064f3c7ca36d0289ea7
joemiddie13 <joemiddie13@gmail.com>
2024-10-03 16:53:40 -0700

    Updated CSS declaration

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,7 @@ module.exports = {
   testEnvironment: 'node',
   collectCoverage: true,
   coverageDirectory: 'coverage',
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy"
+  }
 };
